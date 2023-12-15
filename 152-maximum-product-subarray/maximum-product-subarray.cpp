b40d28090e50cfdd729c885bf1f11e998a53bf8c
joshnagulipilli <104157061+joshnagulipilli@users.noreply.github.com>
2023-12-15 20:19:57 +0530

    Time: 10 ms (16.44%) | Memory: 14 MB (79.92%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -2,24 +2,16 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
          int maxi=INT_MIN;
-         
          int pref=1,suff=1;
          for(int i=0;i<nums.size();i++){
               if(pref==0) pref=1;
                    if(suff==0) suff=1;
 
-                   pref=pref*nums[i];
-                   suff=suff*nums[nums.size()-i-1];
-
+                   pref*=nums[i];
+                   suff*=nums[nums.size()-i-1];
                    maxi=max(maxi,max(pref,suff));
          }
             return maxi;
          }
 };
-auto init = []()
-{ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
+
