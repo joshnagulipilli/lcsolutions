6523a341a6712cbc1b95d3e859625c3c2c5e5150
joshnagulipilli <104157061+joshnagulipilli@users.noreply.github.com>
2023-12-18 15:12:48 +0530

    Time: 67 ms (99.38%) | Memory: 93.8 MB (29.00%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        ios::sync_with_stdio(false);
+    cin.tie(nullptr);
         int m=prices[0],ans=0;
         for(auto i:prices){
             if(m>i) m=i;
@@ -10,9 +12,3 @@ public:
     }
 };
 
-auto init=[](){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
