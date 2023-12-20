c076da283cd7b787bdabdb6639a2f42d46316f4f
joshnagulipilli <104157061+joshnagulipilli@users.noreply.github.com>
2023-12-20 07:52:37 +0530

    Time: 7 ms (91.14%) | Memory: 47.1 MB (13.41%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
         sort(prices.begin(),prices.end());
-    int c=0;
-    int m=money;
+    int c=0 , m=money;
         for(int i=0;i<prices.size();i++){
             if(c==2) break;
             if(prices[i]<=money){
