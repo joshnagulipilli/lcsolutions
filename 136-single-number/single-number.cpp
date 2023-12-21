7fae7527097550463917790da6f4c73ea7935cf3
joshnagulipilli <104157061+joshnagulipilli@users.noreply.github.com>
2023-12-21 11:07:04 +0530

    Time: 7 ms (97.97%) | Memory: 17.2 MB (48.81%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int x1=0;
+        for(int i=0;i<nums.size();i++){
+            x1= x1^nums[i];
+        }
+        return x1;
+    }
+};
