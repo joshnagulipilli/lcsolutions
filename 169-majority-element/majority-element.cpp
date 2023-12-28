0931f2431bedbfe060beebd0d2a59708b2bb0596
joshnagulipilli <104157061+joshnagulipilli@users.noreply.github.com>
2023-12-28 08:46:30 +0530

    Time: 24 ms (5.13%) | Memory: 20.1 MB (30.08%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
+    int majorityElement(vector<int>& v) {
         ios::sync_with_stdio(false);
         cin.tie(0);
-        unordered_map<int,int>m;
-        for(auto i:nums)  m[i]++;
-        for(auto i:m){
-            if(i.second > (nums.size()/2))
-            {
-                return i.first;
+        int n=v.size();
+        int cnt=0;
+        int el;
+        for(int i=0;i<n;i++){
+            if(cnt==0){
+                cnt=1;
+                el=v[i];
             }
+            else if(el==v[i]) cnt++;
+            else cnt--;
         }
+        int cnt2=0;
+        for(int i=0;i<n;i++){
+            if(el==v[i]) cnt2++;
+        }
+        if(cnt2 > n/2) return el;
         return -1;
     }
 };
