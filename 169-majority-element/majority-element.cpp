829233a548252644f528f815d8cd5197e2b1d9ec
joshnagulipilli <104157061+joshnagulipilli@users.noreply.github.com>
2024-02-12 08:16:59 +0530

    Time: 3 ms (99.85%) | Memory: 22.2 MB (18.20%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -7,18 +7,13 @@ public:
         int cnt=0;
         int el;
         for(int i=0;i<n;i++){
-            if(cnt==0){
-                cnt=1;
+            if(cnt==0){             
                 el=v[i];
-            }
+                cnt++;
+            } 
             else if(el==v[i]) cnt++;
             else cnt--;
         }
-        int cnt2=0;
-        for(int i=0;i<n;i++){
-            if(el==v[i]) cnt2++;
-        }
-        if(cnt2 > n/2) return el;
-        return -1;
+        return el;
     }
 };
