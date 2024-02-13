74b3e1301316bb646c0c183b5157c5d44c82835d
joshnagulipilli <104157061+joshnagulipilli@users.noreply.github.com>
2024-02-13 17:04:14 +0530

    Time: 52 ms (37.55%) | Memory: 26.6 MB (36.60%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -5,14 +5,10 @@ bool ispal(string str)
     int i=0,j=str.size()-1;
     while(i<=j)
     {
-        if(str[i]==str[j]) {
-            i++;
-            j--;
-        }
+        if(str[i]==str[j]){  i++;j--; }
         else return false;
     }
     return true;
-
 }
     string firstPalindrome(vector<string>& words) {
         int n=words.size();
