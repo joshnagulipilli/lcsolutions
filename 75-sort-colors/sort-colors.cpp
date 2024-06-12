6fad201d01dc4e2192f55895911d0caac6f5e034
joshnagulipilli <104157061+joshnagulipilli@users.noreply.github.com>
2024-06-12 22:24:47 +0530

    Time: 4 ms (31.04%) | Memory: 9.9 MB (34.14%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
 //dutch national flag
     void sortColors(vector<int>& arr) {
-        int low=0,mid=0,high=arr.size()-1;
-        while(mid<=high){
-            if(arr[mid]==0){
-                swap(arr[low],arr[mid]);
-                low++;
-                mid++;
-            }
-            else if(arr[mid]==1) mid++;
-            else{
-                swap(arr[mid],arr[high]);
-                high--;
-            }
+      int n=arr.size();
+      int l=0,m=0,h=n-1;
+      while(m<=h){
+        if(arr[m]==0){
+            swap(arr[l],arr[m]);
+            m++;
+            l++;
         }
+        else if(arr[m]==1) m++;
+        else {
+            swap(arr[m],arr[h]);
+            h--;
+        }
+      }
     }
 };
