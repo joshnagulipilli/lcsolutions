9b9f6312394dfcb3d9490205be6e7e8206db99c4
joshnagulipilli <104157061+joshnagulipilli@users.noreply.github.com>
2024-06-22 22:10:44 +0530

    Time: 20 ms (97.30%) | Memory: 21.3 MB (64.94%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-int maxelement(vector<int> &v,int n)
+   
+   int maxelement(vector<int> &v,int n)
 {
     int maxi=INT_MIN;
     for(int i=0;i<n;i++)
@@ -10,28 +11,34 @@ int maxelement(vector<int> &v,int n)
     return maxi;
 }
 
-long long int fun(vector<int> &v, int hrs)
-{
-    long long int total=0;
+
+   long long int fun(int hours,vector<int>& v)
+   {
+   long long int total=0;
     for(int i=0;i<v.size();i++)
     {
-        total+=ceil((double)v[i]/(double)hrs);
+        total+=ceil((double)v[i]/(double)hours);
     }
     return total;
-}
+   }
+
+
+
     int minEatingSpeed(vector<int>& v, int h) {
-        int n=v.size();
-        int low=1,high= maxelement(v,n);
-        while(low<=high)
+       int n=v.size();
+       int high=maxelement(v,v.size());
+       int low=1;
+       while(low<=high)
+       {
+        int mid=(low+high)/2;
+        long long int sol=fun(mid,v);
+        if(sol<=h)
         {
-            int mid=low+(high-low)/2;
-            long long int res=fun(v,mid);
-            if(res<=h)
-            {
-                high=mid-1;
-            }
-            else low=mid+1;
+        
+            high=mid-1;
         }
-        return low;
+        else low=mid+1;
+       }
+       return low;
     }
 };
