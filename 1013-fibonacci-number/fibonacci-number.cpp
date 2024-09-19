23829295266f58b1d10470b4fc7fe26c3a77bad8
joshnagulipilli <104157061+joshnagulipilli@users.noreply.github.com>
2024-09-19 15:46:05 +0530

    Time: 15 ms (17.22%) | Memory: 7.4 MB (34.57%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+
+int fun(int n){
+    if(n==0) return 0;
+    if(n==1) return 1;
+    return fib(n-1)+fib(n-2);
+}
+
+
+    int fib(int n) {
+      return  fun(n);
+    }
+};
