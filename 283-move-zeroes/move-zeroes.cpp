c0e58d2f5e20fca125f8fb649a3a3b9534ae2248
joshnagulipilli <104157061+joshnagulipilli@users.noreply.github.com>
2024-09-30 16:25:27 +0530

    Time: 13 ms (75.60%) | Memory: 21.9 MB (27.63%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        ios::sync_with_stdio(false);
-        cin.tie(0);
         int c=0;
         for(int i=0;i<nums.size();i++)
         {
