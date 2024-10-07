bfb0bb08ea1063e16b6bdc28a8957ecaf4cbdbac
joshnagulipilli <104157061+joshnagulipilli@users.noreply.github.com>
2024-10-07 09:24:46 +0530

    Time: 7 ms (51.96%) | Memory: 12.1 MB (7.22%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
     int minLength(string s) {
-         int n=s.length();
-         int i=0;
-         string str="";
-         while(i<s.length())
-         {
-            cout<<i<<" ";
-            if(s[i]=='A' && s[i+1]=='B'   || s[i]=='C' && s[i+1]=='D')
-            {
+        //  int n=s.length();
+        //  int i=0;
+        //  string str="";
+        //  while(i<s.length())
+        //  {
+        //     cout<<i<<" ";
+        //     if(s[i]=='A' && s[i+1]=='B'   || s[i]=='C' && s[i+1]=='D')
+        //     {
                  
-                str=s.substr(0,i);
-                str+=s.substr(i+2);
-                s=str;
-                cout<<str;
-                str="";
-                if(i!=0 )i--;
+        //         str=s.substr(0,i);
+        //         str+=s.substr(i+2);
+        //         s=str;
+        //         cout<<str;
+        //         str="";
+        //         if(i!=0 )i--;
+        //     }
+        //     else i++;
+        //  }
+        //  return s.length();
+        stack<int>st;
+        int i=0;
+        while(i<s.length())
+        {
+            if(st.empty())
+            {
+                st.push(s[i]);
             }
-            else i++;
-         }
-         return s.length();
+            else{
+                if((s[i]=='B' && st.top()=='A') || (s[i]=='D' && st.top()=='C'))
+            {
+                st.pop();
+            }
+            else st.push(s[i]);
+            }
+            i++;
+        }
+        return st.size();
     }
 };
