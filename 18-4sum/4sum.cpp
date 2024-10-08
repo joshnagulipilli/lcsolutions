8d59aefed00d0888f87dadaec9564bffe31eb525
joshnagulipilli <104157061+joshnagulipilli@users.noreply.github.com>
2024-10-08 13:00:48 +0530

    Time: 22 ms (81.31%) | Memory: 16.2 MB (72.39%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -5,9 +5,9 @@ public:
         sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
         for(int i=0;i<n;i++){
-            if(i!=0 && nums[i]==nums[i-1]) continue;
+            if(i>0 && nums[i]==nums[i-1]) continue;
             for(int j=i+1;j<n;j++){
-                if(j!=i+1 && nums[j]==nums[j-1]) continue;
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
                 int k=j+1;
                 int l=n-1;
                 while(k<l){
@@ -15,14 +15,14 @@ public:
                     sum+=nums[k];
                     sum+=nums[l];
                     if(sum==target){
-                        vector<int>temp={nums[i],nums[j],nums[k],nums[l]};
-                        ans.push_back(temp);
+                        
+                        ans.push_back({nums[i],nums[j],nums[k],nums[l]});
                         k++;l--;
                         while(k<l && nums[k]==nums[k-1]) k++;
                         while(k<l && nums[l]==nums[l+1]) l--;
                     }
                     else if(sum<target) k++;
-                        else l--;
+                    else l--;
                 }
             }
         }
