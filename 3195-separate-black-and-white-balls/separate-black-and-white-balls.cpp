3d8741ab7d911de4d4d96b3f7d634b3b92750999
joshnagulipilli <104157061+joshnagulipilli@users.noreply.github.com>
2024-10-15 09:51:24 +0530

    Time: 36 ms (32.00%) | Memory: 15.4 MB (89.89%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long minimumSteps(string s) {
         long long ans=0,cnt=0;
-        for(int i=0;i<s.size();i++)
+        for(int i=0;s[i];i++)
         {
             if(s[i]=='1') cnt++;
             else ans+=cnt;          // cnt defines - number of 1 s to shift from begining
