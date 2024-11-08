12fa3b0473bad5ec6c5f9e98fab9483107da1bdf
joshnagulipilli <104157061+joshnagulipilli@users.noreply.github.com>
2024-11-08 11:43:48 +0530

    Time: 12 ms (7.84%) | Memory: 11.1 MB (55.23%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -3,24 +3,27 @@ public:
     string frequencySort(string s) {
         int n=s.length();
         unordered_map<char,int>mp;
-        for(int i=0;s[i];i++) mp[s[i]]++;
-        vector<pair<int,char>>v;
-        for(auto it:mp)
-        {
-            v.push_back({it.second,it.first});
+        for(auto it:s){
+            mp[it]++;
         }
-        sort(v.begin(),v.end(),   
-        [](auto a , auto b){
-            return a.first>b.first;
-            });
-        string ans="";
-        for(auto it:v)
-        {
-            for(int i=0;i<it.first;i++)
-            {
-                ans.push_back(it.second);
+        vector<pair<int,char>>ans;
+        for(auto it:mp){
+            ans.push_back({it.second,it.first});
+            
+        }
+       
+        sort(ans.begin(),ans.end(),[](auto a,auto b){
+            return a.first > b.first;
+        });
+         for(auto it:ans){
+            cout<<it.first<<" "<<it.second<<endl;
+        }
+        string str ="";
+        for(auto it:ans){
+            for(int i=0;i<it.first;i++){
+                str.push_back(it.second);
             }
         }
-        return ans;
+        return str;
     }
 };
