323fa83e797194f6e848cd5093c2118313d6bda1
joshnagulipilli <104157061+joshnagulipilli@users.noreply.github.com>
2025-12-29 21:40:04 +0530

    Time: 109 ms (53.97%) | Memory: 120.1 MB (42.01%) - LeetSync

--- 575-distribute-candies/distribute-candies.cpp
+++ 575-distribute-candies/distribute-candies.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
         int n=candyType.size();
-        set<int>s;
+        unordered_set<int>s;
         for(int i=0;i<n;i++){
             s.insert(candyType[i]);
         }
